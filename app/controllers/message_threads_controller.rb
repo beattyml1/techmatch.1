5235c5a6f71103635e0e4ddcf0d313d531531925
Prelang Builder <builder@prelang.com>
2016-07-31 21:41:31 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/message_threads_controller.rb
+++ app/controllers/message_threads_controller.rb
@@ -1,4 +1,8 @@
 class MessageThreadsController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_message_thread, only: [:show, :edit, :update, :destroy]
 
   # GET /message_threads
